c98f4f78e67917becee37ff297e9055f0d50b4cd
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-04-10 02:44:52 +0600

    Use Loading, Handling Error and Route-Specific Data Load

--- src/app/foods/[foodId]/page.jsx
+++ src/app/foods/[foodId]/page.jsx
@@ -1,11 +1,40 @@
+import Image from 'next/image';
 import React from 'react';
 
 const FoodDetailPage = async ({ params }) => {
     const { foodId } = await params;
 
+    const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/foods/${foodId}`);
+    const data = await res.json();
+    const food = data.data;
+
+    const {
+        dish_name,
+        image_link,
+        category,
+        alternative_names,
+        main_ingredients,
+        approximate_nutrition_per_serving,
+        rating,
+        price,
+        cuisine,
+        cooking_steps,
+    } = food;
+
     return (
         <div>
-            <h2>Showing details of food: {foodId}</h2>
+            <h2 className='my-5'>Showing details of food: {foodId}</h2>
+
+            <h2>{dish_name}</h2>
+            <p>{price}</p>
+
+            <Image
+                src={image_link}
+                alt={dish_name}
+                width={200}
+                height={200}
+                className="object-cover"
+            />
         </div>
     );
 };
---
+++ src/app/foods/error.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+
+const error = () => {
+    return (
+        <div>
+           <h2 className='mt-25 text-5xl text-red-500 font-bold'>Something went wrong!</h2> 
+        </div>
+    );
+};
+
+export default error;
