9e15eb37b7b067b61c888661c46b47d45e8a3424
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-04-10 00:57:40 +0600

    Recap Data Fetching and Display Data

---
+++ src/app/foods/page.jsx
@@ -0,0 +1,25 @@
+import FoodCard from '@/components/FoodCard';
+import React from 'react';
+
+const FoodsPage = async () => {
+
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/foods");
+    const foodsData = await res.json();
+    const foods = foodsData.data;
+
+    console.log(foods, "foods data");
+
+    return (
+        <div className='max-w-10/12 mx-auto'>
+            <h2>Foods: {foods.length}</h2>
+
+            <div className='grid grid-cols-3 gap-4'>
+                {
+                    foods.map((food) => <FoodCard food={food} key={food.id}></FoodCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default FoodsPage;
